da27dc3363df48c5f1e6671f28347d4b528023f7
Keyhoh <iheyhoh@gmail.com>
2018-09-26 20:55:53 +0900

    completed

--- src/util.js
+++ src/util.js
@@ -1,12 +1,11 @@
 export const createData = (min, max, n, a) => {
     if (a === undefined) a = 0;
-    let len = max - min;
     let data = [];
 
     data.push([0, random(min, max)]);
-    for (let i = i; i < n; i++) {
+    for (let i = 1; i < n; i++) {
         let Max = Math.min(max, data[i - 1][1] + a);
-        let Min = Math.max(min, data[i - 1][1] - 1);
+        let Min = Math.max(min, data[i - 1][1] - a);
         data.push([i, random(Min, Max)]);
     }
     return data;
